6be243c58afa355517a5fb54bf1bdfd78750143d
TAAMEFL2 <florence.meier@swisscom.com>
2018-11-24 21:46:59 +0100

    start on filter

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,55 +1,60 @@
 <template>
   <div>
-     <v-container grid-list-md>
-       <v-layout row>
-      <!-- SEARCH -->
-        <v-flex xs2>
-          TODO FILTERS
+    <v-container class="mx-0">
+      <v-layout row>
+        <v-flex xs3>
+          <v-treeview
+            :items="dateFilter"
+            open-all
+            selectable
+            item-key="name"
+            v-model="selectedFilter"
+          >
+          </v-treeview>
         </v-flex>
-          <v-flex xs10>
-            <v-autocomplete
-                v-model="query"
-                :items="randomMedicalWords"
-                :readonly="!isEditing"
-                @click="toggleEdit()"
-                :search-input.sync="search"
-                append-icon="mdi-magnify"
-                chips
-                multiple
+        <v-flex xs9>
+          <v-autocomplete
+            v-model="query"
+            :items="randomMedicalWords"
+            :readonly="!isEditing"
+            @click="toggleEdit()"
+            :search-input.sync="search"
+            append-icon="mdi-magnify"
+            chips
+            multiple
+          >
+            <template
+              slot="selection"
+              slot-scope="data"
+            >
+              <v-chip
+                :selected="data.selected"
+                close
+                class="chip--select-multi"
+                @input="remove(data.item)"
               >
-              <template
-                slot="selection"
-                slot-scope="data"
-              >
-                <v-chip
-                  :selected="data.selected"
-                  close
-                  class="chip--select-multi"
-                  @input="remove(data.item)"
-                >
-                  {{ data.item }}
-                </v-chip>
-              </template>
-            </v-autocomplete>
-              <!-- END OF SEARCH -->
+                {{ data.item }}
+              </v-chip>
+            </template>
+          </v-autocomplete>
 
-              <!-- RESULTS DISPLAY -->
-              <v-card class="my-2" v-for="article in dummyArticles" :key="article.id">
-                <v-card-title primary-title>
-                  <div>
-                    <div class="headline secondary--text">{{article.title}}</div>
-                    <span v-html="$options.filters.highlight($options.filters.truncateAbstract(article.text), query)">{{article.text | truncateAbstract | highlight(query)}}</span>
-                  </div>
-                  <v-spacer></v-spacer>
-                  <div class="grey--text text-xs-right">
-                    {{article.date}}
-                  </div>
-                </v-card-title>
-            </v-card>
-              <!-- END OF RESULTS DISPLAY -->
-            </v-flex>
-        </v-layout>
-      </v-container>
+          <!-- RESULTS DISPLAY -->
+          <v-card v-if="filteredDate(selectedFilter, article.id), article.show" class="my-2" v-for="article in dummyArticles" :key="article.id">
+            <v-card-title primary-title>
+              <div>
+                <div class="headline secondary--text" v-html="$options.filters.highlight(article.title, query)">{{article.title | highlight(query)}}</div>
+                <span v-html="$options.filters.highlight($options.filters.truncateAbstract(article.text), query)">{{article.text | truncateAbstract | highlight(query)}}</span>
+              </div>
+              <v-spacer></v-spacer>
+              <div class="grey--text text-xs-right">
+                {{article.date}}
+              </div>
+            </v-card-title>
+          </v-card>
+          <!-- END OF RESULTS DISPLAY -->
+        </v-flex>
+      </v-layout>
+    </v-container>
   </div>
 </template>
 
@@ -69,21 +74,47 @@ export default {
         'adenine', 'hair loss', 'protein', 'hydrolic pressure', 'antibodies'
       ],
       dummyArticles: [
-        {id: 0, title: 'Title of example article', text: 'Lorem ipsum dolor sit amet, aspirin sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At glucose eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet...', date: '11 November 2018'},
-        {id: 1, title: 'Title of another article', text: 'Lorem antibodies dolor sit amet, leukemia sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, glucose diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet...', date: '01 December 2011'},
-        {id: 2, title: 'Title of awesome article', text: 'Lorem ipsum dolor sit amet, antibodies sadipscing elitr, sed diam hydrolic eirmod tempor LSD ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet...', date: '06 May 2015'},
-        {id: 3, title: 'Title of dummy article', text: 'Lorem rat dolor sit amet, yeast sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet...', date: '12 March 1977'},
-        {id: 4, title: 'Title of fake article', text: 'Lorem ipsum hydrolic pressure sit amet, consetetur sadipscing elitr, sed diam LSD eirmod tempor invidunt ut hair loss et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet...', date: '07 November 1996'},
-        {id: 5, title: 'Title of medical article', text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet...', date: '22 March 2001'},
-        {id: 6, title: 'Title of article about diabetes', text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed aspirin nonumy eirmod tempor invidunt ut labore et dolore magna yeast erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet...', date: '09 February 2017'},
-        {id: 7, title: 'Title of last article', text: 'Lorem ipsum dolor sit amet, hair loss sadipscing leukemia, sed diam rat eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet...', date: '30 January 2017'}
+        {id: 0, title: 'Title of example aspirin article', text: 'Lorem ipsum dolor sit amet, aspirin sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At glucose eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet...', date: '25 November 2018', show: false},
+        {id: 1, title: 'Title of another article', text: 'Lorem antibodies dolor sit amet, leukemia sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, glucose diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet...', date: '01 December 2011', show: false},
+        {id: 2, title: 'Title of awesome article about LSD', text: 'Lorem ipsum dolor sit amet, antibodies sadipscing elitr, sed diam hydrolic eirmod tempor LSD ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet...', date: '06 May 2015', show: false},
+        {id: 3, title: 'Title of dummy article', text: 'Lorem rat dolor sit amet, yeast sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet...', date: '12 March 1977', show: false},
+        {id: 4, title: 'Title of fake article', text: 'Lorem ipsum hydrolic pressure sit amet, consetetur sadipscing elitr, sed diam LSD eirmod tempor invidunt ut hair loss et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet...', date: '25 November 2018', show: false},
+        {id: 5, title: 'Title of medical article', text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet...', date: '22 March 2001', show: false},
+        {id: 6, title: 'Title of article about diabetes', text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed aspirin nonumy eirmod tempor invidunt ut labore et dolore magna yeast erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet...', date: '09 November 2017', show: false},
+        {id: 7, title: 'Title of last article', text: 'Lorem ipsum dolor sit amet, hair loss sadipscing leukemia, sed diam rat eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet...', date: '30 January 2017', show: false}
       ],
-      query: ''
+      query: '',
+      selectedFilter: []
     }
   },
   watch: {
     search (val) {
       val && this.querySelections(val)
+    }
+  },
+  computed: {
+    dateFilter () {
+      return [
+        {
+          id: 1,
+          name: 'Date',
+          children: [
+            { id: 2, name: 'Today' },
+            { id: 3, name: 'November' },
+            { id: 4, name: '2018' },
+            { id: 5, name: '< 2018' }
+          ]
+        },
+        {
+          id: 6,
+          name: 'Journal',
+          children: [
+            { id: 7, name: 'Science' },
+            { id: 8, name: 'Nature' },
+            { id: 9, name: 'Physics today' }
+          ]
+        }
+      ]
     }
   },
   methods: {
@@ -98,6 +129,25 @@ export default {
     remove (item) {
       const index = this.randomMedicalWords.indexOf(item)
       if (index >= 0) this.randomMedicalWords.splice(index, 1)
+    },
+    filteredDate (selectedFilter, id) {
+      for (let i = 0; i < selectedFilter.length; i++) {
+        if (selectedFilter[i] === 'Today') {
+          if (this.dummyArticles[id].date.includes('25 November 2018')) {
+            this.dummyArticles[id].show = true
+          } else {
+            this.dummyArticles[id].show = false
+          }
+        }
+        if (selectedFilter[i] === 'November') {
+          if (this.dummyArticles[id].date.includes('November')) {
+            this.dummyArticles[id].show = true
+          } else {
+            this.dummyArticles[id].show = false
+          }
+        }
+      }
+      return true
     }
   },
   filters: {
@@ -112,8 +162,8 @@ export default {
       }
     },
     truncateAbstract (value) {
-      if (value.length > 200) {
-        value = value.substring(0, 199) + '...'
+      if (value.length > 350) {
+        value = value.substring(0, 349) + '...'
         return value
       }
     }
